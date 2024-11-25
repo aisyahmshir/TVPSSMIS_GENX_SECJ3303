676f9aa97977276b07a9636bd9882120a09e0ea8
SarveishBalasundram <b.sarveish26@gmail.com>
2024-11-25 21:40:14 +0800

    push example files

--- TVPSS_Core/target/generated-sources/annotations/com/tvpss/controller/HomeController.java
+++ TVPSS_Core/target/generated-sources/annotations/com/tvpss/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.tvpss.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,4 +14,13 @@ public class HomeController {
         model.addAttribute("message", "Welcome to Thymeleaf with Spring MVC!");
         return "home"; // This expects /WEB-INF/views/home.html
     }
+    @GetMapping("/chart")
+    public String getChartData(Model model) {
+        // Example data to display on the chart
+        model.addAttribute("labels", List.of("January", "February", "March", "April", "May"));
+        model.addAttribute("data", List.of(65, 59, 80, 81, 56));
+
+        return "chart";
+    }
+
 }
